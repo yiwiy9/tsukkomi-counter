7c178587c4b02a14e9c575fd39c48acf2925c6fb
yiwiy9 <yiwiy9@gmail.com>
2024-01-26 01:19:49 +0900

    Add zustand and counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
-import { useState } from 'react'
+import { FC, KeyboardEvent } from 'react'
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-function App() {
-  const [count, setCount] = useState(0)
+type CounterState = {
+  count: number
+  updateCount: (count: number) => void
+}
+
+const useCounterStore = create<CounterState>()(
+  persist(
+    (set) => ({
+      count: 0,
+      updateCount: (count) => set(() => ({ count: count })),
+    }),
+    { name: 'tsukkomi-storage' },
+  ),
+)
+
+const App: FC = () => {
+  const { count, updateCount } = useCounterStore()
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+    const key = e.code
+    if (key === 'Space') updateCount(count + 1)
+    else if (key === 'KeyM') updateCount(count - 1)
+    else if (key === 'Escape') updateCount(0)
+  }
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-    </>
+    <div className="flex items-center text-xl font-bold text-white">
+      <input
+        className="w-10 appearance-none bg-transparent text-right caret-transparent focus:outline-none"
+        id="inline-full-name"
+        value={count}
+        onKeyDown={handleKeyDown}
+        readOnly
+      />
+      <label className="ml-1.5" htmlFor="inline-full-name">
+        ツッコミ
+      </label>
+    </div>
   )
 }
 
